d239e37bcc75acff86b2f5a057e111db1aee745a
Victor Giovannoni <vernalhav@usp.br>
2020-08-10 17:14:43 -0300

    Added initial styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import levadaLogo from './assets/images/levada-logo-white.svg';
+
 function App() {
     return (
         <div className="App">
+            <img src={levadaLogo} />
             <h1>Hello World</h1>
         </div>
     );
