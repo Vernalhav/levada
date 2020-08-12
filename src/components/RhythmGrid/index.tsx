4838d5f191553a64c6526aea477eaff920125d68
Victor Giovannoni <vernalhav@usp.br>
2020-08-12 17:03:32 -0300

    Restructure components to not reshuffle on beat add

--- src/components/RhythmGrid/index.tsx
+++ src/components/RhythmGrid/index.tsx
@@ -1,32 +1,25 @@
 import React from 'react';
 
-import RhythmicFigure, { RHYTHMIC_FIGURE_NAMES } from '../RhythmicFigure';
+import RhythmicFigure from '../RhythmicFigure';
 
 import './styles.css';
 
 interface RhythmGridProps {
     currentBeat: number;
-    maxBeats: number;
-}
-
-function getRandomFigure(): string {
-    return RHYTHMIC_FIGURE_NAMES[Math.floor(Math.random() * RHYTHMIC_FIGURE_NAMES.length)];
+    rhythmicFigures: string[];
 }
 
 function RhythmGrid(props: RhythmGridProps): JSX.Element {
-    const rhythmicFigures = [];
-
-    // Make states?
-    for (let i = 0; i < props.maxBeats; i++) rhythmicFigures.push(getRandomFigure());
+    const maxBeats = props.rhythmicFigures.length;
 
     return (
         <div id="rhythm-grid">
             <div className="container">
-                {rhythmicFigures.map((rhythmicFigure: string, i: number) => {
+                {props.rhythmicFigures.map((rhythmicFigure: string, i: number) => {
                     return (
                         <RhythmicFigure
                             type={rhythmicFigure}
-                            isHighlighted={(props.currentBeat - 1) % props.maxBeats === i}
+                            isHighlighted={(props.currentBeat - 1) % maxBeats === i}
                             key={i}
                         />
                     );
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -5,12 +5,29 @@ import beatSound from '../../assets/sounds/beat.wav';
 import snapSound from '../../assets/sounds/snap.wav';
 
 import RhythmGrid from '../../components/RhythmGrid';
+import getRhythmicFigure from '../../utils/getRhythmicFigure';
 
 import './styles.css';
 
 function MainPage(): JSX.Element {
     const [currentBeat, setCurrentBeat] = useState(1);
     const [maxBeats, setMaxBeats] = useState(4);
+    const [rhythmicFigures, setRhythmicFigures] = useState(() => {
+        const randomArray: string[] = [];
+        for (let i = 0; i < maxBeats; i++) randomArray.push(getRhythmicFigure());
+        return randomArray;
+    });
+
+    const beat = new Audio(beatSound);
+    const snap = new Audio(snapSound);
+
+    function handleNewBeat() {
+        setRhythmicFigures([...rhythmicFigures, getRhythmicFigure()]);
+    }
+
+    function handleRemoveBeat() {
+        setRhythmicFigures(rhythmicFigures.slice(0, rhythmicFigures.length - 1));
+    }
 
     return (
         <div id="main-page">
@@ -19,14 +36,14 @@ function MainPage(): JSX.Element {
             </header>
 
             <div className="center-container">
-                <audio src={beatSound} preload="auto" />
-                <audio src={snapSound} preload="auto" />
                 <div className="container">
                     <div className="btn-container">
                         <button
                             type="button"
                             onClick={() => {
                                 setCurrentBeat(currentBeat + 1);
+                                beat.play();
+                                snap.play();
                             }}
                         >
                             Next beat
@@ -36,7 +53,10 @@ function MainPage(): JSX.Element {
                         <button
                             type="button"
                             onClick={() => {
-                                if (maxBeats < 10) setMaxBeats(maxBeats + 1);
+                                if (maxBeats < 10) {
+                                    setMaxBeats(maxBeats + 1);
+                                    handleNewBeat();
+                                }
                             }}
                         >
                             +
@@ -44,7 +64,10 @@ function MainPage(): JSX.Element {
                         <button
                             type="button"
                             onClick={() => {
-                                if (maxBeats > 4) setMaxBeats(maxBeats - 1);
+                                if (maxBeats > 4) {
+                                    setMaxBeats(maxBeats - 1);
+                                    handleRemoveBeat();
+                                }
                             }}
                         >
                             -
@@ -53,7 +76,7 @@ function MainPage(): JSX.Element {
                 </div>
             </div>
 
-            <RhythmGrid currentBeat={currentBeat} maxBeats={maxBeats} />
+            <RhythmGrid currentBeat={currentBeat} rhythmicFigures={rhythmicFigures} />
         </div>
     );
 }
