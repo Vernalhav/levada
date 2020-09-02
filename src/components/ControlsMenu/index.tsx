0a3d8c1a23f28963e5e41b3004ebf864eb40cd88
Victor Giovannoni <vernalhav@usp.br>
2020-09-02 14:45:43 -0300

    Added tooltips for control buttons

--- src/components/ControlsMenu/index.tsx
+++ src/components/ControlsMenu/index.tsx
@@ -3,6 +3,7 @@ import classNames from 'classnames';
 
 import Select from '../Select';
 import FigureSelectBar, { states } from '../FigureSelectBar';
+import Tooltip from '@material-ui/core/Tooltip';
 
 import upArrowIcon from '../../assets/images/icons/up_arrow.svg';
 import downArrowIcon from '../../assets/images/icons/down_arrow.svg';
@@ -71,6 +72,8 @@ function ControlsMenu({
     const BPM_STEP = 10;
     const N_BPMS = (MAX_BPM - MIN_BPM) / BPM_STEP + 1;
 
+    const TOOLTIP_DELAY = 500;
+
     const [currentState, setCurrentState] = useState(states.NONE);
     const [prevState, setPrevState] = useState(states.NONE);
 
@@ -92,79 +95,141 @@ function ControlsMenu({
                     {isPlaying ? 'Stop' : 'Play'}
                 </button>
 
-                <button
-                    className="add-beat-btn"
-                    type="button"
-                    disabled={isAddBeatDisabled}
-                    onClick={() => {
-                        handleNewBeat();
-                    }}
+                <Tooltip
+                    title="Add new beat"
+                    placement="top"
+                    enterNextDelay={TOOLTIP_DELAY}
+                    enterDelay={TOOLTIP_DELAY}
+                    arrow
                 >
-                    <img src={upArrowIcon} alt="Add beat" />
-                </button>
+                    <button
+                        className="add-beat-btn"
+                        type="button"
+                        disabled={isAddBeatDisabled}
+                        onClick={() => {
+                            handleNewBeat();
+                        }}
+                    >
+                        <img src={upArrowIcon} alt="Add beat" />
+                    </button>
+                </Tooltip>
 
-                <button
-                    className="remove-beat-btn"
-                    type="button"
-                    disabled={isRemoveBeatDisabled}
-                    onClick={handleRemoveBeat}
+                <Tooltip
+                    title="Remove last beat"
+                    placement="bottom"
+                    enterNextDelay={TOOLTIP_DELAY}
+                    enterDelay={TOOLTIP_DELAY}
+                    arrow
                 >
-                    <img src={downArrowIcon} alt="Remove beat" />
-                </button>
+                    <button
+                        className="remove-beat-btn"
+                        type="button"
+                        disabled={isRemoveBeatDisabled}
+                        onClick={handleRemoveBeat}
+                    >
+                        <img src={downArrowIcon} alt="Remove beat" />
+                    </button>
+                </Tooltip>
 
-                <button
-                    className={classNames({ mute: true, 'is-muted': isMuted })}
-                    disabled={areControlsDisabled}
-                    onClick={handleMuteToggle}
+                <Tooltip
+                    title={isMuted ? 'Play whole rhythm' : 'Play only metronome'}
+                    placement="top"
+                    enterNextDelay={TOOLTIP_DELAY}
+                    enterDelay={TOOLTIP_DELAY}
+                    arrow
                 >
-                    <img src={isMuted ? volumeOffIcon : volumeOnIcon} alt={isMuted ? 'Unmute' : 'Mute'} />
-                </button>
+                    <button
+                        className={classNames({ mute: true, 'is-muted': isMuted })}
+                        disabled={areControlsDisabled}
+                        onClick={handleMuteToggle}
+                    >
+                        <img src={isMuted ? volumeOffIcon : volumeOnIcon} alt={isMuted ? 'Unmute' : 'Mute'} />
+                    </button>
+                </Tooltip>
 
-                <button
-                    className="randomize"
-                    type="button"
-                    disabled={areControlsDisabled || areNoFiguresSelected}
-                    onClick={handleRandomizeBeats}
+                <Tooltip
+                    title="Randomize current sequence"
+                    placement="top"
+                    enterNextDelay={TOOLTIP_DELAY}
+                    enterDelay={TOOLTIP_DELAY}
+                    arrow
                 >
-                    <img src={randomizeIcon} alt="Randomize beats" />
-                </button>
+                    <button
+                        className="randomize"
+                        type="button"
+                        disabled={areControlsDisabled || areNoFiguresSelected}
+                        onClick={handleRandomizeBeats}
+                    >
+                        <img src={randomizeIcon} alt="Randomize beats" />
+                    </button>
+                </Tooltip>
 
-                <button
-                    className={classNames({ loop: true, 'isnt-looping': !isLooping })}
-                    type="button"
-                    disabled={areControlsDisabled}
-                    onClick={handleLoopToggle}
+                <Tooltip
+                    title={isLooping ? 'Stop after rhythm ends' : 'Loop rhythm'}
+                    placement="top"
+                    enterNextDelay={TOOLTIP_DELAY}
+                    enterDelay={TOOLTIP_DELAY}
+                    arrow
                 >
-                    <img src={isLooping ? loopIcon : noLoopIcon} alt="Toggle loop rhythm" />
-                </button>
+                    <button
+                        className={classNames({ loop: true, 'isnt-looping': !isLooping })}
+                        type="button"
+                        disabled={areControlsDisabled}
+                        onClick={handleLoopToggle}
+                    >
+                        <img src={isLooping ? loopIcon : noLoopIcon} alt="Toggle loop rhythm" />
+                    </button>
+                </Tooltip>
 
-                <button
-                    className={classNames({ 'allow-figures': true, 'is-active': currentState === states.SELECT })}
-                    type="button"
-                    disabled={areControlsDisabled || (currentState !== states.NONE && currentState !== states.SELECT)}
-                    onClick={() => {
-                        setPrevState(currentState);
-                        currentState === states.NONE ? setCurrentState(states.SELECT) : setCurrentState(states.NONE);
-                    }}
+                <Tooltip
+                    title={currentState === states.SELECT ? 'Close pane' : 'Select which rhythmic figures can appear'}
+                    placement="bottom"
+                    enterNextDelay={TOOLTIP_DELAY}
+                    enterDelay={TOOLTIP_DELAY}
+                    arrow
                 >
-                    <img src={selectIcon} alt="Choose which figures can appear" />
-                </button>
+                    <button
+                        className={classNames({ 'allow-figures': true, 'is-active': currentState === states.SELECT })}
+                        type="button"
+                        disabled={
+                            areControlsDisabled || (currentState !== states.NONE && currentState !== states.SELECT)
+                        }
+                        onClick={() => {
+                            setPrevState(currentState);
+                            currentState === states.NONE
+                                ? setCurrentState(states.SELECT)
+                                : setCurrentState(states.NONE);
+                        }}
+                    >
+                        <img src={selectIcon} alt="Choose which figures can appear" />
+                    </button>
+                </Tooltip>
 
-                <button
-                    className={classNames({ 'choose-figure': true, 'is-active': currentState === states.CHOOSE })}
-                    type="button"
-                    disabled={
-                        areControlsDisabled ||
-                        (areMaxBeatsSelected && currentState !== states.CHOOSE) ||
-                        (currentState !== states.NONE && currentState !== states.CHOOSE)
-                    }
-                    onClick={() => {
-                        setPrevState(currentState);
-                        currentState === states.NONE ? setCurrentState(states.CHOOSE) : setCurrentState(states.NONE);
-                    }}
+                <Tooltip
+                    title={currentState === states.CHOOSE ? 'Close pane' : 'Choose next rhythmic figure'}
+                    placement="bottom"
+                    enterNextDelay={TOOLTIP_DELAY}
+                    enterDelay={TOOLTIP_DELAY}
+                    arrow
                 >
-                    <img src={addIcon} alt="Choose new rhythmic figure" />
-                </button>
+                    <button
+                        className={classNames({ 'choose-figure': true, 'is-active': currentState === states.CHOOSE })}
+                        type="button"
+                        disabled={
+                            areControlsDisabled ||
+                            (areMaxBeatsSelected && currentState !== states.CHOOSE) ||
+                            (currentState !== states.NONE && currentState !== states.CHOOSE)
+                        }
+                        onClick={() => {
+                            setPrevState(currentState);
+                            currentState === states.NONE
+                                ? setCurrentState(states.CHOOSE)
+                                : setCurrentState(states.NONE);
+                        }}
+                    >
+                        <img src={addIcon} alt="Choose new rhythmic figure" />
+                    </button>
+                </Tooltip>
 
                 <div className="bpm">
                     <Select
